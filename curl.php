33ccf569d4c1d9855441ebdf2dee1f85befe5670
easycodescript <68246535+easycodescript@users.noreply.github.com>
2023-02-15 16:58:23 +0100

    weather in console.log

---
+++ curl.php
@@ -0,0 +1,28 @@
+<?php
+echo getWeather($_GET['q']);
+
+function getWeather($location){
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://api.openweathermap.org/data/2.5/weather?q=$location&units=metric&appid=042559fc8f13bd0e86e557aa02965a24",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+        "x-rapidapi-host: community-open-weather-map.p.rapidapi.com",
+        "x-rapidapi-key: [your rapidapi key]"
+    ),
+    ));
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+    if ($err) {
+        return "cURL Error #:" . $err;
+    } else {
+        return $response;
+    }
+}
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8" />
   <title>Coding Test Weather Client</title>
   <link href="style.css" rel="stylesheet" />
+  <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
 </head>
 <body>
   <main class="app">
@@ -16,5 +17,19 @@
     </article>
   </main>
   <script src="weather.js"></script>
+  <script>
+    $(function() {
+      console.clear();
+      console.log('it works!');
+      $('#search-input').on('input', function(){
+        const searchInputValue = $('#search-input').val();
+        $.get( './curl.php?q=' + searchInputValue , function( data ) {
+           console.log(JSON.parse(data));
+          
+        });
+      })
+           
+    });
+  </script>
 </body>
 </html>
