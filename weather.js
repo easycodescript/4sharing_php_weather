9ed6ba4323dfcd13f23ad12b2040aab5e6962fad
easycodescript <68246535+easycodescript@users.noreply.github.com>
2023-07-27 13:04:45 +0200

    spinner, css & js changes

--- weather.js
+++ weather.js
@@ -1,18 +1,42 @@
-const searchInput$ = document.getElementById('search-input');
+$(function() {
+  $('#home-icon').click(function(){
+    showIt('' , '', '', '');
+    $('#search-input').val(null);
+    $('#search-input').focus();
+  })
+  $('#search-input').on('input', function(){
+    $('#search-icon').hide();
+    $('#spinner').show();
+    const searchInputValue = $('#search-input').val();
+    $.ajaxSetup({
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+      }
+    })
+    $.get( './curl.php?q=' + searchInputValue , function( data ) {
+       const jsonResponse = JSON.parse(data);
+       console.log(jsonResponse);
+       if (jsonResponse.weather) {
+          showIt(jsonResponse.name,
+            jsonResponse.weather[0].main + ', ' + jsonResponse.main.temp + '°',
+            jsonResponse.weather[0].description,
+            jsonResponse.weather[0].icon
+          )
+       } else {
+        showIt('Nothing found :(' , '', '', '');
+       }
+    });
+  })
+       
+});
 
-const fetchForecast = (location) => {
-  // @TODO fetch entered location and inject into DOM
-};
-
-let delayTimer;
-const onSearchInputChange = (event) => {
-  if (event && event.currentTarget && event.currentTarget.value) {
-    const enteredLocation = event.currentTarget.value;
-    clearTimeout(delayTimer);
-    delayTimer = setTimeout(() => {
-      fetchForecast(enteredLocation);
-    }, 400);
-  }
-};
-
-searchInput$.addEventListener('input', onSearchInputChange);
+function showIt(city, main, description, icon) {
+  $('#weather__city').html(city);
+  $('#weather__main').html(main);
+  $('#weather__description').html(description);
+  const iconUrl= icon != '' ? "http://openweathermap.org/img/wn/" + icon + "@4x.png" : null;
+  $('#weather__icon').attr("src", iconUrl)
+  $('#weather__icon').show();
+  $('#spinner').hide();
+  $('#search-icon').show();
+}
